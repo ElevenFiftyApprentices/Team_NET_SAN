c978598d2099cbfb3fcf2cbb8c92e1320f34ff75
Shelby Grice <shelbygrice25@gmail.com>
2017-05-17 13:14:05 -0400

    Created models

---
+++ Shopping List/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping_List.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ShoppingListId { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset ModifiedUtc { get; set; }
+    }
+}
---
+++ Shopping List/Models/ShoppingListItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping_List.Models
+{
+    public enum Priority
+    {
+        Need,
+        Want,
+        Later
+    }
+    public class ShoppingListItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ShoppingListId { get; set; }
+
+        public string Contents { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsChecked { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset ModifiedUtc { get; set; }
+    }
+}
